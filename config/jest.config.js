8a71a5c145bc5b3f3d42500df9e8b0de18a2c144
Dr Wells <welldoneesu0@gmail.com>
2025-08-02 08:47:36 +0100

    save changes

--- config/jest.config.js
+++ config/jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   setupFilesAfterEnv: ['./jest.setup.js'],
   testTimeout: 10000,
+  moduleNameMapper: {
+    '^./config/db$': '<rootDir>/__mocks__/db.js',
+  },
 };
